67fd08536266afbcd30743d24ccb1fbed9de6cd2
Dani <d.tarazoma@geekshubs.com>
2022-06-04 17:39:32 +0200

    feat: comment user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router()
 
-const UserController = require('../controllers/UserController');
+// const UserController = require('../controllers/UserController');
 const authController = require('../controllers/authController');
 const auth = require('../middleware/verifyToken');
 
-router.get('/users', auth,  UserController.getAllUsers);
+// router.get('/users', auth,  UserController.getAllUsers);
 
-router.get('/user/:id', auth, UserController.getUserById);
+// router.get('/user/:id', auth, UserController.getUserById);
 
-router.put('/user/:id', auth, UserController.updateUserByid);
+// router.put('/user/:id', auth, UserController.updateUserByid);
 
-router.delete('/user/:id', auth, UserController.deleteUserByid);
+// router.delete('/user/:id', auth, UserController.deleteUserByid);
 
 router.post('/auth/login', authController.login);
 router.post('/auth/register', authController.register);
