e02607bc582014b9c19be1298f5321444e815d4f
Dani <d.tarazoma@geekshubs.com>
2022-06-08 16:45:22 +0200

    feat: add cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 // const morgan = require('morgan');
 require('dotenv').config();
+const cors = require('cors');
 
 const tasks = require('./routes/task.routes');
 
@@ -8,9 +9,18 @@ const app = express();
 const { mongoose } = require('./config/database');
 
 const port = process.env.PORT || 3000;
+let corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  // methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+  preflightContinue: false,
+  // allowedHeaders: "Origin,X-Requested-With,Content-Type,Accept,Authorization",
+  optionsSuccessStatus: 204
+};
 
 // app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
   return res.json({
